e94c31f27b5822cb45c798f88f91b3ac1d2acddd
Arpitthedamager <sg79720@gmail.com>
2025-03-17 14:15:39 +0530

    seo done

--- src/app/components/home/home.tsx
+++ src/app/components/home/home.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import AboutSection from "../main/About";
 import BlogSection from "../main/blog/Bilog";
 import ClientSuccessStories from "../main/ClientStories";
@@ -13,24 +14,49 @@ import ShowcaseExpertise from "../main/ShowcaseExpertise";
 import Team from "../main/team/Team";
 
 const HomePage: React.FC = () => {
-    return (
-       <>
-        <div className="bg-black min-h-screen">
-         <Navbar />
-         <HeroSection />
-         <AboutSection />
-         <DevelopmentSection />
-         <ShowcaseExpertise />
-         <ProcessSection />
-         <PricingPage />
-         <Team />
-         <ClientSuccessStories />
-         <FAQ />
-         <BlogSection/>
-         <Cta/>
-         <Footer />
-       </div></>
-    );
+  return (
+    <>
+      {/* ✅ SEO Metadata */}
+      <Head>
+        <title>WebTechStudio - Leading Web Development Agency in Agra, India</title>
+        <meta name="description" content="WebTechStudio is a top web development agency in Agra, India, specializing in modern websites, UI/UX design, and SEO-friendly web solutions." />
+        <meta name="keywords" content="Web Development, UI/UX Design, SEO, Web Design, Agra, India, WebTechStudio" />
+        <meta name="author" content="WebTechStudio" />
+
+        {/* ✅ Open Graph (For Social Media) */}
+        <meta property="og:title" content="WebTechStudio - Leading Web Development Agency in Agra, India" />
+        <meta property="og:description" content="Get high-quality websites and digital solutions from WebTechStudio, the top web development company in Agra, India." />
+        <meta property="og:image" content="/mainpage/webtechstudio-og-image.jpg" />
+        <meta property="og:url" content="https://webtechstudio.com/" />
+        <meta property="og:type" content="website" />
+
+        {/* ✅ Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="WebTechStudio - Leading Web Development Agency in Agra, India" />
+        <meta name="twitter:description" content="We create high-performing websites and digital solutions for businesses of all sizes in Agra, India." />
+        <meta name="twitter:image" content="/mainpage/webtechstudio-twitter-image.jpg" />
+
+        {/* ✅ Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="bg-black min-h-screen">
+        <Navbar />
+        <HeroSection />
+        <AboutSection />
+        <DevelopmentSection />
+        <ShowcaseExpertise />
+        <ProcessSection />
+        <PricingPage />
+        <Team />
+        <ClientSuccessStories />
+        <FAQ />
+        <BlogSection />
+        <Cta />
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
