8637c577f79e53021c3293214ec749e580621c2a
Arpitthedamager <sg79720@gmail.com>
2025-03-20 15:18:34 +0530

    add additional paths to sitemap for improved SEO and content indexing

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://webtechstudio.site', // Replace with your site's URL
-  generateRobotsTxt: true, // (Optional) Generate a robots.txt file
-  // Additional options can be added here
-}
+  siteUrl: 'https://webtechstudio.site', // Your website URL
+  generateRobotsTxt: true, // Generates robots.txt
+
+  additionalPaths: async (config) => {
+    return [
+      { loc: '/Blog/nextjs-performance-optimization', changefreq: 'weekly', priority: 0.8 },
+      { loc: '/Blog/animation-framer-motion-gsap', changefreq: 'weekly', priority: 0.8 },
+      { loc: '/Blog/ui-ux-design-nextjs-framer-motion-gsap', changefreq: 'weekly', priority: 0.8 },
+      { loc: '/Blog/ecommerce-website-development-nextjs-framer-motion-gsap', changefreq: 'weekly', priority: 0.8 },
+      { loc: '/Blog/seo-nextjs', changefreq: 'weekly', priority: 0.8 },
+      { loc: '/Blog/performance-optimization-nextjs-framer-motion-gsap', changefreq: 'weekly', priority: 0.8 },
+    ];
+  },
+};
