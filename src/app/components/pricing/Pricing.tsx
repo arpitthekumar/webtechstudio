8f297dc4f91eacf7c0a16a3148220d7efa32aaeb
Arpitthedamager <sg79720@gmail.com>
2025-04-10 02:19:07 +0530

    fix: restore import for MaintenancePricing component in PricingPage

--- src/app/components/pricing/Pricing.tsx
+++ src/app/components/pricing/Pricing.tsx
@@ -2,10 +2,10 @@ import ClientSuccessStories from "../main/ClientStories";
 import Cta from "../main/cta/Cta";
 import FAQ from "../main/faq";
 import Footer from "../main/Footer";
-import MaintenancePricing from "./components/mpricing/MPricing";
 import Navbar from "../main/Navbar";
 import PricingPag from "../main/Pricing";
 import GymPOSPricing from "./components/gympospricing/GymPOSPricing";
+import MaintenancePricing from "./components/mpricing/MPricing";
 
 const PricingPage: React.FC = () => {
   return (
