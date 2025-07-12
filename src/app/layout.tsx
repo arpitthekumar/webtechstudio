b1df8079676866301b0713fbab0f5c8b55e40f74
arpitthekumar <sg79720@gmail.com>
2025-07-12 11:24:57 +0530

    feat: add Microsoft Clarity tracking script to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,6 +88,17 @@ export default function RootLayout({
           content="avlaj4hsl80raElN6J_6Do1K39M50iybjbBXr-HebmM"
         />
         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "sdlqym662t");
+            `,
+          }}
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
